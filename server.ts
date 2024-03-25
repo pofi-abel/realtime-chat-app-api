5978f8714931315a1e2d19448b0969af6b7a56bc
Pofi Abel <abel@barnksfortetech.com>
2024-03-25 11:56:02 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,22 +1,19 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import connectMongoDb from "./db/connect";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import connectMongoDb from "./db/connect";
 
 import authRoutes from "./routes/auth.route";
 import messageRoutes from "./routes/message.route";
 import userRoutes from "./routes/uesr.route";
 
-import NotFoundMiddleware from "./middlewares/not-found.middleware";
 import errorHandlerMiddleware from "./middlewares/error-handler.middleware";
+import NotFoundMiddleware from "./middlewares/not-found.middleware";
 
-import { app, server } from "./socket/socket";
-import path from "path";
 import httpStatus from "http-status";
+import { app, server } from "./socket/socket";
 
 const port = process.env.PORT || 4000;
-
-const dirname = path.resolve();
 
 dotenv.config();
 
@@ -30,11 +27,6 @@ app.use("/", (req: Request, res: Response) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-app.use(express.static(path.join(dirname, "/frontend/dist")));
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(dirname, "frontend", "dist", "index.html"));
-});
 
 app.use(NotFoundMiddleware);
 app.use(errorHandlerMiddleware);
