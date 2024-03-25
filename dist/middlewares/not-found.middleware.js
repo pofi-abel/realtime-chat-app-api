c01e1c6d46f679c27276085bfbaab54f19ac09d1
Pofi Abel <abel@barnksfortetech.com>
2024-03-25 12:03:22 +0100

    ignore dist

--- dist/middlewares/not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_1 = __importDefault(require("http-status"));
-const NotFoundMiddleware = (req, res) => res.status(http_status_1.default.NOT_FOUND).send(`[Route does not exist]: ${req.url} `);
-exports.default = NotFoundMiddleware;
