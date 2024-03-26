ad87f00cf1d2e07dfd8c1764ab1ec77f2f9c61b0
Pofi Abel <abel@barnksfortetech.com>
2024-03-26 09:46:57 +0100

    Update server.ts
    increased request limit
    added allow proxy

--- server.ts
+++ server.ts
@@ -16,9 +16,11 @@ import { app, server } from "./socket/socket";
 const port = process.env.PORT || 4000;
 
 dotenv.config();
-
-app.use(express.json());
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
+
+app.set("trust proxy", true);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
